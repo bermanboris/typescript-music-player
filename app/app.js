cdce789cc62a7be3fbdbb4e1b9a7423bc51c8047
Boris Berman <boris@berman.solutions>
2016-04-29 19:56:18 +0300

    company constructor with interface

--- app/app.js
+++ app/app.js
@@ -1,6 +1,17 @@
 // Declaring variable with specific type
 var personName = "Boris";
-// This example will work in ECMAScript 6 but fail in TypeScript
-// Error TS2322: Type 'number' is not assignable to type 'string'.
-personName = 123;
+function printCompanyData(company) {
+    console.log("Incoming data...  " + company.name + ": <" + company.email + " - " + company.phone + "> ");
+}
+var Company = (function () {
+    function Company(name, phone, email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+    return Company;
+}());
+var google = new Company("Google", 5551234321, "a@google.com");
+printCompanyData(google);
+// console.log(company); 
 //# sourceMappingURL=app.js.map
