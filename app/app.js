1139be211750a9db844713ae7702862b09b2667b
Boris Berman <boris@berman.solutions>
2016-04-29 19:35:38 +0300

    added basic files to project

---
+++ app/app.js
@@ -0,0 +1,6 @@
+// Declaring variable with specific type
+var personName = "Boris";
+// This example will work in ECMAScript 6 but fail in TypeScript
+// Error TS2322: Type 'number' is not assignable to type 'string'.
+personName = 123;
+//# sourceMappingURL=app.js.map
---
+++ app/app.ts
@@ -0,0 +1,7 @@
+// Declaring variable with specific type
+var personName:string = "Boris";
+
+// This example will work in ECMAScript 6 but fail in TypeScript
+// Error TS2322: Type 'number' is not assignable to type 'string'.
+personName = 123;
+
